9122d42c9a09cf93db1433530df7b79a58f86062
Boju Siva <169000154+Boju2024@users.noreply.github.com>
2026-04-11 13:38:57 -0400

    added remote service protocol

---
+++ Sources/SBDomain/Transactions/Contracts/SBTransactionRemoteService.swift
@@ -0,0 +1,14 @@
+//
+//  SBTransactionRemoteService.swift
+//  SBDomain
+//
+//  Created by Sivakumar Boju on 4/10/26.
+//  Copyright (c) 2026 Near 22. All rights reserved
+//
+
+import Foundation
+
+public protocol SBTransactionRemoteService: Sendable {
+    func fetchTransactions() async throws -> [SBTransaction]
+    func fetchTransactions(since postedDate: String) async throws -> [SBTransaction]
+}
